9ade4df1559b6b52b4b34d57e73128e233d8ff0b
Axcel Rodriguez <146502446+AxcelDevlp@users.noreply.github.com>
2023-10-30 09:32:09 -0600

    calculator.py

--- calculator.py
+++ calculator.py
@@ -1,15 +1,26 @@
-# Main functions (input)
+# Main function (input)
 def main():
-    user_input = input("Enter your operation: ")
+    user_input = input(
+        "Enter your operation ('+' for Addition, '-' for Subtraction, '*' for Multiplication,\n'/' for Division, '%' for Modulus, '**' for Exponentiation and '//' for Floor division): "
+    )
 
     # Split the values(3)
     values = user_input.split()
 
+    # Check if there are exactly 3 values
+    if len(values) != 3:
+        print("SYNTAX ERROR")
+        return main()
+
     # Naming the variables
-    if len(values) >= 3:
-        num1 = values[0]
-        op = values[1]
-        num2 = values[2]
+    num1 = values[0]
+    op = values[1]
+    num2 = values[2]
+
+    # Check if num1 and num2 are valid numbers
+    if not num1.isdigit() or not num2.isdigit():
+        print("ERROR. Both number 1 and number 2 should be numbers.")
+        return main()
 
     # Convert the variables to int(num1 and num2)
     num1 = int(num1)
@@ -27,6 +38,10 @@ def main():
         print(num1 / num2)
     elif op == "*":
         print(num1 * num2)
+    elif op == "%":
+        print(num1 % num2)
+    elif op == "**":
+        print(num1**num2)
     else:
         print("SYNTAX ERROR")
         return main()
